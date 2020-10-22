ca2fa3313b22bd93e118c4b19d574965cdb9521b
Jaime Bea <jaimebeapz@gmail.com>
2020-10-22 15:33:55 +0200

    Template Vec3

--- Vec3Exercise/main.cpp
+++ Vec3Exercise/main.cpp
@@ -3,23 +3,23 @@
 
 int main()
 {
-	Vec3 v1 = Vec3();
+	Vec3<float> v1 = Vec3<float>();
 	std::cout << "Default constructor:\n";
 	std::cout << "(" << v1.x << ", " << v1.y << ", " << v1.z << ")\n";
 
-	Vec3 v2 = Vec3(1, 2, 3);
+	Vec3<float> v2 = Vec3<float>(1, 2, 3);
 	std::cout << "Non-default constructor:\n";
 	std::cout << "(" << v2.x << ", " << v2.y << ", " << v2.z << ")\n";
 
-	Vec3 v3 = Vec3(v2);
+	Vec3<float> v3 = Vec3<float>(v2);
 	std::cout << "Copy:\n";
 	std::cout << "(" << v3.x << ", " << v3.y << ", " << v3.z << ")\n";
 
-	Vec3 v4 = v2 + Vec3(4, 3, 2);
+	Vec3<float> v4 = v2 + Vec3<float>(4, 3, 2);
 	std::cout << "Addition:\n";
 	std::cout << "(" << v4.x << ", " << v4.y << ", " << v4.z << ")\n";
 
-	Vec3 v5 = v2.Normalized();
+	Vec3<float> v5 = v2.Normalized();
 	std::cout << "Normalization:\n";
 	std::cout << "(" << v5.x << ", " << v5.y << ", " << v5.z << ")\n";
 
@@ -35,7 +35,7 @@ int main()
 	std::cout << "Dot product:\n";
 	std::cout << "(" << dot << ")\n";
 
-	Vec3 v6 = v2.CrossProduct(v4);
+	Vec3<float> v6 = v2.CrossProduct(v4);
 	std::cout << "Cross product:\n";
 	std::cout << "(" << v6.x << ", " << v6.y << ", " << v6.z << ")\n";
 
--- Vec3Exercise/vec3.h
+++ Vec3Exercise/vec3.h
@@ -1,19 +1,49 @@
 #pragma once
 
+template <class T>
 struct Vec3
 {
-	Vec3();
-	Vec3(const Vec3& other);
-	Vec3(float x, float y, float z);
+	T x, y, z;
 
-	Vec3 operator+(const Vec3& other) const;
+	Vec3() : Vec3(0, 0, 0) {}
 
-	Vec3 Normalized() const;
-	float Length() const;
-	float DistanceTo(const Vec3& other) const;
-	float DotProduct(const Vec3& other) const;
-	Vec3 CrossProduct(const Vec3& other) const;
-	float AngleBetween(const Vec3& other) const;
+	Vec3(const Vec3<T>& other) : Vec3(other.x, other.y, other.z) {}
 
-	float x, y, z;
+	Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
+
+	inline Vec3<T> operator+(const Vec3<T>& other) const
+	{
+		return Vec3(x + other.x, y + other.y, z + other.z);
+	}
+
+	inline Vec3<T> Normalized() const
+	{
+		float length = Length();
+		return Vec3(x / length, y / length, z / length);
+	}
+
+	inline float Length() const const
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+
+	inline float DistanceTo(const Vec3<T>& other) const
+	{
+		return sqrt((other.x - x) * (other.x - x) + (other.y - y) * (other.y - y) + (other.z - z) * (other.z - z));
+	}
+
+	inline float DotProduct(const Vec3<T>& other) const
+	{
+		return x * other.x + y * other.y + z * other.z;
+	}
+
+	inline Vec3<T> CrossProduct(const Vec3<T>& other) const
+	{
+		return Vec3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
+	}
+
+	inline float AngleBetween(const Vec3<T>& other) const
+	{
+		return acos(DotProduct(other) / (Length() * other.Length()));
+	}
 };
