bd3703352b5b4b7acd05e9b815ca8d2bccd0d205
Jaime Bea <jaimebeapz@gmail.com>
2020-10-22 21:47:49 +0200

    Optimize Vec3

--- Vec3Exercise/vec3.h
+++ Vec3Exercise/vec3.h
@@ -5,51 +5,53 @@ struct Vec3
 {
 	T x, y, z;
 
-	Vec3() : Vec3(0, 0, 0) {}
+	Vec3() {}
 
-	Vec3(const Vec3<T>& other) : Vec3(other.x, other.y, other.z) {}
+	Vec3(const Vec3<T>& other) : x(other.x), y(other.y), z(other.z) {}
 
 	Vec3(T new_x, T new_y, T new_z) : x(new_x), y(new_y), z(new_z) {}
 
-	inline Vec3<T> operator+(const Vec3<T>& other) const
+	Vec3<T> operator+(const Vec3<T>& other) const
 	{
 		return Vec3(x + other.x, y + other.y, z + other.z);
 	}
 
-	inline Vec3<T> Normalized() const
+	Vec3<T> Normalized() const
 	{
 		float length = Length();
+
 		if (length > -FLT_EPSILON && length < FLT_EPSILON)
 		{
 			return Vec3(x, y, z);
 		}
-		else
-		{
-			return Vec3(x / length, y / length, z / length);
-		}
+		
+		return Vec3(x / length, y / length, z / length);
 	}
 
-	inline float Length() const const
+	float Length() const const
 	{
 		return sqrt(x * x + y * y + z * z);
 	}
 
-	inline float DistanceTo(const Vec3<T>& other) const
+	float DistanceTo(const Vec3<T>& other) const
 	{
-		return sqrt((other.x - x) * (other.x - x) + (other.y - y) * (other.y - y) + (other.z - z) * (other.z - z));
+		float x_diff = other.x - x;
+		float y_diff = other.y - y;
+		float z_diff = other.z - z;
+		return sqrt(x_diff * x_diff + y_diff * y_diff + z_diff * z_diff);
 	}
 
-	inline float DotProduct(const Vec3<T>& other) const
+	float DotProduct(const Vec3<T>& other) const
 	{
 		return x * other.x + y * other.y + z * other.z;
 	}
 
-	inline Vec3<T> CrossProduct(const Vec3<T>& other) const
+	Vec3<T> CrossProduct(const Vec3<T>& other) const
 	{
 		return Vec3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
 	}
 
-	inline float AngleBetween(const Vec3<T>& other) const
+	float AngleBetween(const Vec3<T>& other) const
 	{
 		return acos(DotProduct(other) / (Length() * other.Length()));
 	}
